1fa69f11222026ece1d6ccaa75aad514141dbeec
Payman Tohidifar <payman.tohidifar@gmail.com>
2026-08-01 16:13:28 -0700

    Fix random sampling in prepare_dataset function by updating random_split to random_state

--- src/gpt2_classifier/data.py
+++ src/gpt2_classifier/data.py
@@ -191,7 +191,7 @@ def prepare_dataset(
         read_kwargs["names"] = spec.column_names
 
     df = pd.read_csv(raw_path, **read_kwargs)
-    df = df.sample(frac=hold_frac, random_split=123)
+    df = df.sample(frac=hold_frac, random_state=123)
     df = df[[spec.text_column, spec.label_column]].rename(
         columns={spec.text_column: _NORMALIZED_TEXT_COLUMN, spec.label_column: _NORMALIZED_LABEL_COLUMN}
     )
